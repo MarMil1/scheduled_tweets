40f991288fda5966cf736d1e8e740408676031ce
marmil1 <milosavjevic.marko@outlook.com>
2024-05-21 11:32:25 -0500

    Implement sidekiq job scheduler, change server timezone, config sidekiq for dev and prod.

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -1,12 +1,16 @@
 class TweetJob < ApplicationJob
   queue_as :default
 
-  def perform(tweet)
+  def perform(tweet_id)
+    tweet = Tweet.find_by(id: tweet_id)
+    return unless tweet # Tweet not found, exit gracefully
+    
     return if tweet.published?
-
-    # Rescheduled a tweet to the future
     return if tweet.publish_at > Time.current
-
+  
     tweet.publish_to_twitter!
+  rescue ActiveRecord::RecordNotFound
+    # Handle the case where the Tweet record is not found
+    Rails.logger.error("Tweet with ID #{tweet_id} not found")
   end
 end
